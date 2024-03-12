a9048e17bf35e2ef41e20f459ffb2ca6108bca2f
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-03-12 16:58:22 +0100

    esercizio 2 manca una funzione

--- src/esercizio2/Esercizio2.java
+++ src/esercizio2/Esercizio2.java
@@ -1,4 +1,31 @@
 package esercizio2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Esercizio2 {
+
+    public static List<Integer> generaNumeriRandomOrdinata(int numeri) {
+        List<Integer> listaNumeriRandom = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < numeri; i++) {
+            listaNumeriRandom.add(random.nextInt(1, 100));
+        }
+        Collections.sort(listaNumeriRandom);
+        return listaNumeriRandom;
+    }
+
+    public static List<Integer> generaListaRandomInversa(List<Integer> listaNumeri) {
+        Collections.reverse(listaNumeri);
+        return listaNumeri;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> numeriCasuali = generaNumeriRandomOrdinata(30);
+        System.out.println(numeriCasuali);
+
+    }
 }
