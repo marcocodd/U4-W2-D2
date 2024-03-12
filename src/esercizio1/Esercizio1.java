b9a9aa97e2dea6864bd444b26ee83bbf3fc43d79
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-03-12 16:31:17 +0100

    Initial

---
+++ src/esercizio1/Esercizio1.java
@@ -0,0 +1,40 @@
+package esercizio1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Quante parole vuoi inserire?");
+
+        int nParole = input.nextInt();
+
+        input.nextLine();
+
+        Set<String> paroleInserite = new HashSet<>();
+        Set<String> paroleDuplicate = new HashSet<>();
+        for (int i = 0; i < nParole; i++) {
+            System.out.println("Scrivi la parola da inserire n:" + (i + 1));
+            String parola = input.nextLine();
+            paroleInserite.add(parola);
+            if (paroleInserite.contains(parola)) {
+                paroleDuplicate.add(parola);
+
+            }
+
+
+        }
+
+        for (String parola : paroleDuplicate) {
+            System.out.println("parole duplicate " + parola);
+        }
+        for (String parola : paroleInserite) {
+            System.out.println("Parole inserite" + parola);
+        }
+    }
+}
